342b169b0f8db745e5acc26c059626facfa13501
e-lajeunesse <eric.lajeunesse@outlook.com>
2021-03-09 09:59:00 -0500

    GET and POST books

---
+++ BookLover.Models/BookModels/BookDetail.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookLover.Models.BookModels
+{
+    public class BookDetail
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public double AverageRating { get; set; }
+
+        //public BookshelfDisplay ReccomendedBooks { get; set; }
+    }
+}
---
+++ BookLover.Models/BookModels/BookListItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookLover.Models.BookModels
+{
+    public class BookListItem
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public double AverageRating { get; set; }
+    }
+}
