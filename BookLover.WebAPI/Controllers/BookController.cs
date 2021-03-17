317fb60ed0e414fb910159830437d3aa150f1578
e-lajeunesse <eric.lajeunesse@outlook.com>
2021-03-17 13:56:13 -0400

    Add AuthorDisplayItem to BookshelfDispaly

--- BookLover.WebAPI/Controllers/BookController.cs
+++ BookLover.WebAPI/Controllers/BookController.cs
@@ -69,6 +69,7 @@ namespace BookLover.WebAPI.Controllers
         }
 
         [HttpPost]
+        
         public IHttpActionResult PostBookByTitleAndAuthor(string title,string authorName)
         {
             try
